5d9f07155c3a6de659415d25fd6d1a9c7205c868
Marhefka, Istvan <istvan.marhefka@swisscom.com>
2016-10-11 23:33:38 +0200

    adding config + make app configurable

--- src/main/java/org.tddblog.calculator/app/App.java
+++ src/main/java/org.tddblog.calculator/app/App.java
@@ -3,6 +3,8 @@ package org.tddblog.calculator.app;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.tddblog.calculator.calc.CalculatorService;
 
@@ -10,7 +12,10 @@ public class App extends Application<AppConfig> {
     private Injector injector;
 
     public static void main(String[] args) throws Exception {
-        new App().run("server", "config.yml");
+        if (args.length != 1) {
+            System.out.println("Please provide exactly one argument for configuration. See config/config-istvan.yml");
+        }
+        new App().run("server", args[0]);
     }
 
     public App() {
@@ -22,11 +27,17 @@ public class App extends Application<AppConfig> {
     }
 
     @Override
+    public void initialize(Bootstrap<AppConfig> bootstrap) {
+        bootstrap.addBundle(new AssetsBundle("/assets", "/", "index.html"));
+    }
+
+    @Override
     public void run(AppConfig config,
                     Environment environment) {
 
         injector = createInjector(config);
 
+        environment.jersey().setUrlPattern("/api/*");
         environment.jersey().register(injector.getInstance(CalculatorService.class));
         environment.healthChecks().register("dummy", injector.getInstance(AppHealthCheck.class));
     }
