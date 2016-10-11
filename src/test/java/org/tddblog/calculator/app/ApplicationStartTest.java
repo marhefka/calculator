ce3cae0a7094da64916f316c67be8479c7bd8fb7
Marhefka, Istvan <istvan.marhefka@swisscom.com>
2016-10-11 14:04:54 +0200

    fixing broken tests

--- src/test/java/org/tddblog/calculator/app/ApplicationStartTest.java
+++ src/test/java/org/tddblog/calculator/app/ApplicationStartTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ApplicationStartTest {
     @Test
     public void applicationStartsSuccessfully() throws Exception {
-        new App().run("server", "src/test/resources/config-test.yml");
+        new App().run("server", "src/test/resources/config-dropwizard-test.yml");
 
         HttpResponse<String> response = Unirest.post("http://localhost:9000/calculator/createCalculator").asString();
         assertThat(response.getStatus()).isEqualTo(200);
